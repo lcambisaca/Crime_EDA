0c8184bf4ba6e96753a1c5bb22c4ee79dac8d0ab
lcambisaca <leocambisaca183@gmail.com>
2025-01-26 19:04:07 +0000

    First day Done fixing data

--- Crime.py
+++ Crime.py
@@ -1,1 +1,25 @@
-import panda
+import pandas as pd
+import numpy as np
+
+#import folium
+#import matplotlib.pyplot as plt
+#from folium.plugins import HeatMap
+# Load the data
+data = pd.read_csv("Crime_DataSet.csv", dtype={'Perpetrator Age': 'str'})
+columns_to_drop = ['Record ID', 'Agency Code', 'Agency Name', 'Record Source', 'Perpetrator Count', 'Victim Count']
+data.drop(columns=columns_to_drop, inplace=True)
+data.replace('Unknown', np.nan, inplace=True)
+data.dropna(inplace=True)
+
+print(data.head())
+print(data.info()) 
+data.to_csv('full_data_output.csv', index=False)
+
+
+#data.drop_duplicates(inplace=True)
+
+
+
+
+#data['Perpetrator Age'] = data['Perpetrator Age'].astype(str)
+
